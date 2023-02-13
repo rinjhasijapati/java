4839337da2d2ad458e4ade71dd3d5ff2f6f773e0
rinjhasijapati <aastha_sijapati@yahoo.com>
2023-02-13 20:39:19 +0545

    first commit

---
+++ Rinjha.java
@@ -0,0 +1,9 @@
+public class Rinjha {
+    public static void main(String[] args) {
+        System.out.println("My name is Rinjha Sijapati.");
+        System.out.println("I was born in the year 2002.");
+        System.out.println("I live in Lalitpur.");
+        System.out.println("I am a Bachelor Student.");
+        System.out.println("I am studying BIT.");
+    }
+}
---
+++ RinjhaS.java
@@ -0,0 +1,15 @@
+public class RinjhaS {
+    public static void main(String[] args) {
+        System.out.println("Name of the organization: Kamalpokhari Internet Cafe\n\n");
+
+        System.out.println("Address:");
+        System.out.println("----State: Kathmandu");
+        System.out.println("----Zip Code: 2905");
+        System.out.println("----Country: Nepal\n\n");
+
+        System.out.println("Contact information:");
+        System.out.println("----Mobile Number: 9876543210");
+        System.out.println("----Email: net.cafe@gmail.com");
+
+    }
+}
