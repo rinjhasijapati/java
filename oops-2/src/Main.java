dc3d50ddadce099c848eef35cd2ff99ae9b49d9e
rinjhasijapati <aastha_sijapati@yahoo.com>
2023-03-01 18:53:09 +0545

    oops concept

---
+++ oops-2/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter your name:");
+            String name=input.nextLine();
+            System.out.println("Enter your age:");
+            int age=input.nextInt();
+            Person object= new Person(name,age);
+            //System.out.println("name: "+object.name);
+            //System.out.println("Age: "+object.Age);
+            object.introduce();
+    }
+}
