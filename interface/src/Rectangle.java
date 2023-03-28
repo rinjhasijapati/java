d8bb63345ad8740e24471efe2a05225e3df47c5d
rinjhasijapati <aastha_sijapati@yahoo.com>
2023-03-28 19:12:35 +0545

    interface

---
+++ interface/src/Rectangle.java
@@ -0,0 +1,8 @@
+public class Rectangle implements Shape,AnotherShape{
+    public void getArea(){
+        System.out.println("Rectangle Area: ");
+    }
+    public void getColor(){
+        System.out.println("Rectangle's Color: ");
+    }
+}
